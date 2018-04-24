224105493801238f2e5a1ca66a253b15c78f6ae7
girvain <gavinross88@hotmail.co.uk>
2018-04-24 16:23:36 +0100

    finished!!!

--- app/src/main/java/com/example/gavinross/gotravelscotland/tour_activities/TourActivity.java
+++ app/src/main/java/com/example/gavinross/gotravelscotland/tour_activities/TourActivity.java
@@ -204,6 +204,9 @@ public class TourActivity extends AppCompatActivity{
             }
         });
 
+        /*
+        OVERIDE THIS METHOD
+         */
         nextActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
